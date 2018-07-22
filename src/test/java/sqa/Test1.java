734c326d9bb963fb4738c4c956b426bef82ec4f2
AmitKukade <amitkukade3@gmail.com>
2018-07-22 16:57:33 +0530

    My second commit in repo

---
+++ src/test/java/sqa/Test1.java
@@ -0,0 +1,14 @@
+package sqa;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello");
+
+
+	System.out.println("Print Hello");
+
+	}
+
+}
---
+++ src/test/java/sqa/Test2.java
@@ -0,0 +1,12 @@
+package sqa;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Class 2 test");
+
+	}
+
+}
